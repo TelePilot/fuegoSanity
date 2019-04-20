e00d696861c2d4e9a305568c8c336c2ee612727d
Viktor Bjerre <viktor.bjerre@gmail.com>
2019-04-20 13:00:06 +0200

    addin header to content schema

---
+++ schemas/header.js
@@ -0,0 +1,28 @@
+export default {
+  title: "Header",
+  name: "header",
+  type: "document",
+  fields: [
+    {
+      title: "Logo",
+      name: "logo",
+      type: "image",
+    },
+    {
+      title: "Email",
+      name: "email",
+      type: "string",
+    },
+    {
+      title: "Phone number",
+      name: "phone",
+      type: "number",
+    },
+    {
+      title: "Menu",
+      name: "menu",
+      type: "array",
+      of: [{type: 'menuItem'}]
+    }
+  ]
+}
--- schemas/schema.js
+++ schemas/schema.js
@@ -3,13 +3,17 @@ import createSchema from 'part:@sanity/base/schema-creator'
 
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
+import header from './header.js'
+import menuItem from './menuItem.js'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
-  name: 'default',
+  name: 'fuegoSchema',
   // Then proceed to concatenate our our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
+    header
+    menuItem
   ])
 })
